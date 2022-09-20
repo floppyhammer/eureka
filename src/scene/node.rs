11596a85c375315da3c8440e45009ebff66f1eb9
floppyhammer <tannhauser_chen@outlook.com>
2022-09-20 15:32:07 +0800

    Store nodes in indextree arena instead of a vector

--- src/scene/node.rs
+++ src/scene/node.rs
@@ -23,42 +23,38 @@ pub trait AsNode {
 }
 
 pub struct World {
-    // This vector is of type Box<dyn AsNode>, which is a trait object;
+    // Type Box<dyn AsNode> is a trait object;
     // it’s a stand-in for any type inside a Box that implements the AsNode trait.
-    pub nodes: Vec<Box<dyn AsNode>>,
 
     // Scene tree.
-    pub arena: Arena<u64>,
+    pub arena: Arena<Box<dyn AsNode>>,
 
     gizmo: Gizmo,
 }
 
 impl World {
     pub fn new() -> Self {
-        let mut nodes: Vec<_> = Vec::new();
-
         let mut arena = Arena::new();
 
         let gizmo = Gizmo::new();
 
         Self {
-            nodes,
             arena,
             gizmo,
         }
     }
 
     pub fn add_node(&mut self, new_node: Box<dyn AsNode>) -> NodeId {
-        let id = self.arena.new_node(self.nodes.len() as u64);
-        self.nodes.push(new_node);
+        let id = self.arena.new_node(new_node);
+        // self.nodes.push(new_node);
 
         id
     }
 
     pub fn input(&mut self, input_event: &InputEvent) {
         // Handle input.
-        for node in self.nodes.iter_mut() {
-            node.input(input_event);
+        for node in self.arena.iter_mut() {
+            node.get_mut().input(input_event);
         }
     }
 
@@ -70,8 +66,8 @@ impl World {
         singletons: Option<&Singletons>,
     ) {
         // Update nodes.
-        for node in self.nodes.iter_mut() {
-            node.update(&queue, dt, &render_server, singletons);
+        for node in self.arena.iter_mut() {
+            node.get_mut().update(&queue, dt, &render_server, singletons);
         }
     }
 
@@ -82,8 +78,8 @@ impl World {
         singletons: &'b Singletons,
     ) {
         // Draw nodes.
-        for node in self.nodes.iter() {
-            node.draw(render_pass, render_server, singletons);
+        for node in self.arena.iter_mut() {
+            node.get_mut().draw(render_pass, render_server, singletons);
         }
 
         self.gizmo.draw(render_pass, render_server, singletons);
