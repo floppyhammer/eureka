5d2720644299b594d75d4240d0ef1c6e2ce30da9
floppyhammer <tannhauser_chen@outlook.com>
2022-09-29 17:55:51 +0800

    Add font bitmap debugging

--- src/resource/font.rs
+++ src/resource/font.rs
@@ -2,21 +2,35 @@ use std::fs;
 use std::fs::File;
 use std::io::Read;
 use std::path::Path;
-use cgmath::Vector4;
+use cgmath::{Point2, Vector4};
 use fontdue;
 use unicode_segmentation::UnicodeSegmentation;
 
 pub(crate) struct Grapheme {
+    /// This can also be used as an unique ID in the font atlas image.
     text: String,
+    /// Rect box w.r.t. baseline.
+    layout: Vector4<i32>,
+    /// Outlines' bbox.
     bounds: Vector4<f32>,
-    outline_bounds: Vector4<f32>,
-    bitmap: Vec<u8>,
+    /// Region in the font atlas.
+    region: Vector4<u32>,
 }
+
+const FONT_ATLAS_SIZE: u32 = 2096;
 
 pub(crate) struct DynamicFont {
     glyphs: Vec<Grapheme>,
+
     font: fontdue::Font,
+
+    /// Font size.
     size: u32,
+
+    /// Contains all the existing graphemes' bitmaps.
+    atlas_image: image::GrayImage,
+    /// Defines how big is the single grid in the atlas.
+    max_grapheme_size: Point2<f32>,
 }
 
 impl DynamicFont {
@@ -34,24 +48,49 @@ impl DynamicFont {
             glyphs: vec![],
             font,
             size: 24,
+            atlas_image: image::GrayImage::new(FONT_ATLAS_SIZE, FONT_ATLAS_SIZE),
+            max_grapheme_size: Point2::new(0.0, 0.0),
         }
     }
 
     pub(crate) fn get_graphemes(&self, text: String) -> Vec<Grapheme> {
-        let graphemes = vec![];
+        let mut graphemes = vec![];
 
         for g in text.graphemes(true) {
             log::info!("Grapheme: {}", g);
         }
 
         for c in text.chars() {
-            // Rasterize and get the layout metrics for the letter 'g' at 17px.
+            // Rasterize and get the layout metrics for the character.
             let (metrics, bitmap) = self.font.rasterize(c, self.size as f32);
 
             log::info!("Character: {} {:?}", c, metrics);
-            // let grapheme = {
-            //     text: c
+
+            let buffer: &[u8] = &bitmap;
+
+            // For debugging.
+            // if metrics.width * metrics.height > 0 {
+            //     image::save_buffer(&Path::new(&(format!("debug_output/{}.png", c.to_string()))),
+            //                        buffer,
+            //                        metrics.width as u32,
+            //                        metrics.height as u32,
+            //                        image::ColorType::L8).unwrap();
             // }
+
+            let grapheme = Grapheme {
+                text: c.to_string(),
+                layout: Vector4::new(metrics.xmin,
+                                     metrics.ymin,
+                                     metrics.xmin + metrics.width as i32,
+                                     metrics.ymin + metrics.height as i32),
+                bounds: Vector4::new(metrics.bounds.xmin,
+                                     metrics.bounds.ymin,
+                                     metrics.bounds.xmin + metrics.bounds.width,
+                                     metrics.bounds.ymin + metrics.bounds.height),
+                region: Vector4::new(0, 0, 0, 0),
+            };
+
+            graphemes.push(grapheme);
         }
 
         graphemes
