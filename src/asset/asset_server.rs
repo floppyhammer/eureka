01daf1275a7e49e92cfada93f3378729cd4d23c9
floppyhammer <tannhauser_chen@outlook.com>
2024-01-26 15:09:03 +0800

    Bump some libs

--- src/asset/asset_server.rs
+++ src/asset/asset_server.rs
@@ -1,10 +1,10 @@
-use assets_manager::{loader, Asset, AssetCache, AssetGuard, Compound, Handle};
+use assets_manager::{loader, Asset, AssetCache, Compound, Handle};
 use std::collections::HashMap;
 use std::path::PathBuf;
 
 pub struct AssetServer {
     pub asset_dir: PathBuf,
-    asset_cache: AssetCache,
+    pub asset_cache: AssetCache,
 }
 
 impl AssetServer {
@@ -20,20 +20,6 @@ impl AssetServer {
             asset_dir,
             asset_cache: cache,
         }
-    }
-
-    pub fn load<A: Compound>(&mut self, id: &str) -> Option<Handle<A>> {
-        // Get a handle on the asset.
-        let handle = self.asset_cache.load::<A>(id).ok()?;
-
-        // // Lock the asset for reading.
-        // // Any number of read locks can exist at the same time,
-        // // but none can exist when the asset is reloaded.
-        // let asset = handle.read();
-        //
-        // han
-
-        Some(handle)
     }
 
     /// Monitor asset changes.
