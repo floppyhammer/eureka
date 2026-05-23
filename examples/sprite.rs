7b813b95cc72ab5999d4fec05c19ab8c95a18579
floppyhammer <tannhauser_chen@outlook.com>
2026-05-23 23:05:12 +0800

    Remove camera rotation in the sprite example

--- examples/sprite.rs
+++ examples/sprite.rs
@@ -16,8 +16,7 @@ fn main() {
         let world = &mut app.world;
 
         // Add a 2D camera
-        let mut camera = Camera2d::default();
-        camera.transform.rotation = 35.0;
+        let camera = Camera2d::default();
         world.add_node(Box::new(camera), None);
 
         // Texture paths
@@ -29,13 +28,14 @@ fn main() {
 
         // Add a sprite with texture
         let mut sprite1 = Sprite2d::at_path(tree_path.clone());
+        sprite1.set_position(Vec2::new(200f32, 200f32));
         sprite1.custom_update = Some(custom_update);
-        world.add_node(Box::new(sprite1), None);
+        let sprite1_id = world.add_node(Box::new(sprite1), None);
 
         // Add another sprite with the same texture (only loaded once)
         let mut sprite2 = Sprite2d::at_path(tree_path);
         sprite2.set_position(Vec2::new(200f32, 200f32));
-        world.add_node(Box::new(sprite2), None);
+        world.add_node(Box::new(sprite2), Some(sprite1_id));
 
         // Add third sprite with a different texture
         let mut sprite3 = Sprite2d::at_path(texture_path);
